5c0a9201df5622ed0bb387452eae6667078ded7c
jole <mortalt072@gmail.com>
2021-11-30 01:40:56 -0300

    mas progreso

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -145,18 +145,18 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
-  if(num1 > 0 && num1 > num2 && num1 > num3){
-    return "Número 1 es mayor y positivo"
-   }
+  if(num1 === 0 || num2 === 0 || num3 === 0){
+     return "Error"
+  }
   else if(num1 < 0 || num2 < 0 || num3 < 0){
      return "Hay negativos"
-    }
-  else if(num3 > num1 && num3 > num2){
-    return (num3 + 1)
-  }else if(num1 === 0 || num2 === 0 || num3 === 0){
-    return "Error"
   }
-  else return false 
+  else if(num3 > num1 && num3 > num2){
+     return (num3 + 1)
+  }
+  else if(num1 > 0 && num1 > num2 && num1 > num3){
+     return "Número 1 es mayor y positivo"
+  } return false
 }
 
 
@@ -166,13 +166,14 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
-  if(numero === 2 || numero === 3 || numero === 7 || numero === 5){
-    return "true"
-  } 
-  else if(numero % 2 !== 0 && numero % 3 !== 0 && numero % 5 !== 0 && numero % 7 !== 0){
-    return "true"
-  }else {
-  return "false"}
+ if(numero === 0 || numero ===1){
+   return false 
+ }
+ for(let i = 2 ; i < numero ; i++){
+   if(numero % i === 0){
+     return false
+   }
+ } return true
 }
 
 
@@ -192,8 +193,8 @@ function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí
-  for(let i = 6 ; i < 61 ; i= i + 6){
-    console.log([i])
+  for(let i = 0 ; i < 61 ; i= i + 6){
+    return array[i]
   }
   
 }
@@ -201,7 +202,7 @@ function tablaDelSeis(){
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  
+if( return length = ~~(Math.log(numero) / Math.LN10 + 1);)
 }
 
 function doWhile(numero) {
